6df42fbd4b23e64d10ce551c2a1583c873325819
renataa <116390061+reeenatamc@users.noreply.github.com>
2025-10-03 12:35:35 -0500

    FINAL MODELADO DE FINANCES

--- finances/admin.py
+++ finances/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+from finances.models import Transaction, Account, TransactionCategory
+
+@admin.register(Transaction)
+class TransactionAdmin(ModelAdmin):
+    list_display = ('user','amount', 'date')
+
+@admin.register(Account)
+class AccountAdmin(ModelAdmin):
+    list_display = ('user', 'balance')
+
+@admin.register(TransactionCategory)
+class TransactionCategoryAdmin(ModelAdmin):
+    list_display = ('user',)
--- finances/models.py
+++ finances/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from django.conf import settings
 
-class CategoryTransaction(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+class TransactionCategory(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100)
-    svg_image = models.ImageField(upload_to = 'category_icon')
+    svg_image = models.ImageField(upload_to = 'finances/category_icon')
 
     def __str__(self):
         return self.name
+
+class Account(models.Model):
+    class AccountTypes(models.TextChoices):
+        GOAL = 'goal', 'Goal'
+        SAVINGS = 'savings', 'Savings'
+        NORMAL = 'normal', 'Normal'
+        CUSTOM = 'custom', 'Custom'
+
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
+    type = models.CharField(choices=AccountTypes.choices, max_length=10)
 
 class Transaction(models.Model):
     class TransactionTypes(models.TextChoices):
@@ -23,25 +35,11 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     date = models.DateField(auto_now_add=True)
     note = models.TextField(blank=True, null=True)
-    category = models.ForeignKey(CategoryTransaction, on_delete=models.CASCADE)
+    category = models.ForeignKey(TransactionCategory, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user} - {self.transaction_type} {self.amount} on {self.date}"
 
-
-class Account(models.Model):
-    class AccountTypes(models.TextChoices):
-        GOAL = 'goal', 'Goal'
-        SAVINGS = 'savings', 'Savings'
-        NORMAL = 'normal', 'Normal'
-        CUSTOM = 'custom', 'Custom'
-
-    name = models.CharField(max_length=100)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    balance = models.DecimalField(max_digits=12, decimal_places=2)
-    type = models.CharField(choices=AccountTypes.choices, max_length=10)
-
-    def __str__(self):
-        return f"{self.user} - {self.balance}"
 
 
